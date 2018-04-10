048bb02a176d0c11b087daa6bcf8bf8ecd6fe5a4
Gagong <belyaevroma@yandex.ru>
2018-04-10 17:29:50 +0300

    Delete GameVersionUpdater.js

--- src/res/injectables/GameVersionUpdater.js
+++
@@ -1,4 +0,0 @@
-window.setInterval(function() {
-  var updatever = document.getElementById("preloader").getGameVersion();
-  document.dispatchEvent(new CustomEvent("getVer", {detail: updatever}));
-}, 300);
