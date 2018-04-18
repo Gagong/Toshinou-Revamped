4d26f624a915018fdcd7582bb3aac49755fdfffb
ThePunisher <lukab95@hotmail.it>
2018-04-18 11:36:22 +0200

    Code optimization

--- src/js/preferences/Settings.js
+++ src/js/preferences/Settings.js
@@ -1,19 +1,20 @@
 class Settings {
-  constructor(moveRandomly, lockNpc, lockPlayers, autoattack, killNpcs, pause, palladium, bonusbox, cargobox, matherials, runfromenemy, avoidAttackedNPCs, alpha, beta, gamma, delta, zeta, kappa, hades, kuiper, booty, rbooty, bbooty, msqbooty) {
+  constructor(moveRandomly, lockNpc, lockPlayers, autoAttack, killNpcs, pause, palladium, bonusBox, cargoBox, material, runFromEnemy, alpha, beta, gamma, delta, zeta, kappa, hades, kuiper, greenAndGoldBooty, redBooty, blueBooty, masqueBooty, blackBooty) 
+  {
     this._moveRandomly = moveRandomly === true;
     this._lockNpc = lockNpc === true;
     this._lockPlayers = lockPlayers === true;
-    this._autoattack = autoattack === true;
+    this._autoAttack = autoAttack === true;
     this._killNpcs = killNpcs === true;
     this._npcs = [];
     this.npcCircleRadius = 500;
     this.reviveLimit = 5;
     this._pause = pause === true;
     this._palladium = palladium === true;
-    this._bonusbox = bonusbox === true;
-    this._matherials = matherials === true;
-    this._runfromenemy = runfromenemy === true;
-    this._avoidAttackedNPCs = avoidAttackedNPCs === true;
+    this._bonusBox = bonusBox === true;
+    this._cargoBox = cargoBox === true;
+    this._material = material === true;
+    this._runFromEnemy = runFromEnemy === true;
     this._alpha = alpha === true;
     this._beta = beta === true;
     this._gamma = gamma === true;
@@ -22,10 +23,11 @@ class Settings {
     this._kappa = kappa === true;
     this._hades = hades === true;
     this._kuiper = kuiper === true;
-    this._booty = booty === true;
-    this._rbooty = rbooty === true;
-    this._bbooty = bbooty === true;
-    this._msqbooty = msqbooty === true;
+    this._greenAndGoldBooty = greenAndGoldBooty === true;
+    this._redBooty = redBooty === true;
+    this._blueBooty = blueBooty === true;
+    this._masqueBooty = masqueBooty === true;
+    this._blackBooty = blackBooty === true;
   }
 
   get autoAttack() {
@@ -68,67 +70,67 @@ class Settings {
     this._redBooty = value === true;
   }
 
-  get Alpha() {
+  get alpha() {
     return this._alpha;
   }
 
-  set Alpha(value) {
+  set alpha(value) {
     this._alpha = value === true;
   }
 
-  get Beta() {
+  get beta() {
     return this._beta;
   }
 
-  set Beta(value) {
+  set beta(value) {
     this._beta = value === true;
   }
 
-  get Gamma() {
+  get gamma() {
     return this._gamma;
   }
 
-  set Gamma(value) {
+  set gamma(value) {
     this._gamma = value === true;
   }
   
-  get Delta() {
+  get delta() {
     return this._delta;
   }
 
-  set Delta(value) {
+  set delta(value) {
     this._delta = value === true;
   }
-  
-  get Zeta() {
+
+  get zeta() {
     return this._zeta;
   }
 
-  set Zeta(value) {
+  set zeta(value) {
     this._zeta = value === true;
   }
   
-  get Kappa() {
+  get kappa() {
     return this._kappa;
   }
 
-  set Kappa(value) {
+  set kappa(value) {
     this._kappa = value === true;
   }
   
-  get Hades() {
+  get hades() {
     return this._hades;
   }
 
-  set Hades(value) {
+  set hades(value) {
     this._hades = value === true;
   }
   
-  get Kuiper() {
+  get kuiper() {
     return this._kuiper;
   }
 
-  set Kuiper(value) {
+  set kuiper(value) {
     this._kuiper = value === true;
   }
 
