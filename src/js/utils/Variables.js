27e70f11a841ccc75508b087c8565605af0e38de
ButtmanNumbaVan <37773641+ButtmanNumbaVan@users.noreply.github.com>
2018-06-02 23:11:04 +0300

    Client apdat for beta (reload page to fix flash crashing)

--- src/js/utils/Variables.js
+++ src/js/utils/Variables.js
@@ -1,94 +1,94 @@
 class Variables {
 
   static get boxType() {
-    return "_-r1z";
+    return "_-52g";
   }
 
   static get attackerId() {
-    return "_-41m";
+    return "_-B20";
   }
 
   static get attackHp() {
-    return "_-X4B";
+    return "_-F4A";
   }
 
   static get attackShd() {
-    return "_-v3s";
+    return "_-j1T";
   }
 
   static get attackedId() {
-    return "_-a22";
+    return "_-32B";
   }
 
   static get moveDuration() {
-    return "_-L2x";
+    return "_-u19";
   }
 
   static get shipDestoyedId() {
-    return "_-J4S";
+    return "_-Q2v";
   }
 
   static get heroInitMaxHp() {
-    return "_-P2W";
+    return "_-A39";
   }
 
   static get heroInitMaxShd() {
-    return "_-Y3Y";
+    return "_-31K";
   }
 
   static get heroInitHp() {
-    return "_-i30";
+    return "_-42z";
   }
 
   static get hpUpdateMaxHp() {
-    return "_-k4n";
+    return "_-I1D";
   }
 
   static get hpUpdateHp() {
-    return "_-da";
+    return "_-44U";
   }
 
   static get selectMaxHp() {
-    return "_-k4n";
+    return "_-I1D";
   }
 
   static get selectMaxShd() {
-    return "_-Y3Y";
+    return "_-31K";
   }
 
   static get selectHp() {
-    return "_-da";
+    return "_-44U";
   }
 
   static get clanDiplomacy() {
-    return "_-z12";
+    return "_-H4";
   }
 
   static get resource() {
-    return "_-x3E";
+    return "_-Zy";
   }
 
   static get resourceType() {
-    return "_-74g";
+    return "_-O2r";
   }
 
   static get gateType() {
-    return "_-xs";
+    return "_-A2B";
   }
 
   static get gateId() {
-    return "_-Uo";
+    return "_-D3D";
   }
 
   static get assetCreateX() {
-    return "_-N2Y";
+    return "_-G3L";
   }
 
   static get assetCreateY() {
-    return "_-d1U";
+    return "_-I2W";
   }
 
   static get battlestationClanDiplomacy() {
-    return "_-n4z";
+    return "_-K4H";
   }
 }
